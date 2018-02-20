e3a280d677a1306723f15a1eccd209d149ae15bf
Vaios Papaspyros <b.papaspyros@gmail.com>
2018-02-20 19:03:06 +0200

    Changes in random generator

--- src/random/random_generator.hpp
+++ src/random/random_generator.hpp
@@ -2,33 +2,43 @@
 #define RANDOM_GENERATOR_HPP
 
 #include <iostream>
+#include <random/rand_utils.hpp>
 #include <random>
 #include <type_traits>
 
 namespace tools {
 
-    static std::random_device random_device_;
-    static std::mt19937 mt(random_device_());
+    class RandomGenerator {
+    public:
+        RandomGenerator() : _rgen(randutils::auto_seed_128{}.base()) {}
+        std::mt19937 gen() const { return _rgen; }
+
+    private:
+        std::mt19937 _rgen;
+    };
 
     template <typename T, typename std::enable_if<std::is_floating_point<T>::value, int>::type = 0>
     T random_in_range(T lower_bound, T upper_bound)
     {
         std::uniform_real_distribution<T> distribution(lower_bound, upper_bound);
-        return distribution(mt);
+        auto gen = RandomGenerator().gen();
+        return distribution(gen);
     }
 
     template <typename T, typename std::enable_if<std::is_integral<T>::value, int>::type = 0>
     T random_in_range(T lower_bound, T upper_bound)
     {
         std::uniform_int_distribution<T> distribution(lower_bound, upper_bound);
-        return distribution(mt);
+        auto gen = RandomGenerator().gen();
+        return distribution(gen);
     }
 
     template <typename T, typename std::enable_if<std::is_floating_point<T>::value, int>::type = 0>
     T normal_random_in_range(T lower_bound, T upper_bound)
     {
         std::normal_distribution<T> distribution(lower_bound, upper_bound);
-        return distribution(mt);
+        auto gen = RandomGenerator().gen();
+        return distribution(gen);
     }
 
     int8_t random_sgn()
